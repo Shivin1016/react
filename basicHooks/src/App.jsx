25ce33309d48fc7ed79367ab1a01c0ff101ed84d
SHIVANI PRAJAPATI <shivaniprajapati10jan@gmail.com>
2024-11-23 15:07:35 +0530

    update to basic Hook

---
+++ basicHooks/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter , setCounter] = useState(0);
+  const addValue = ()=> setCounter(counter + 1);
+  const removeValue = ()=>{
+     counter === 0 ? setCounter(0) : setCounter(counter - 1);
+  };
+  // let counter = 1;
+
+  // const addValue = ()=>{
+  //   counter++;
+  // }
+  // const removeValue = ()=>{
+  //   counter--;
+  // }
+
+  return (
+    // there is a problem that one vaiable changed but its reflection cannot seen 
+    //we have to take id , calls etc for reflection
+    //That is why react provide hooks which shows the reflection at all places where we use it
+    // without using id , class etc...
+    //so if changes occure at UI then React will come into Picture
+    //react update this counter value at all places where it uses
+    <div className="container">
+      <h1>Hooks React</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}> Add value   </button>
+      <button onClick={removeValue} > Remove value </button>
+      <footer>counter : {counter}</footer>
+    </div>
+  );
+}
+
+export default App;
